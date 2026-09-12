cf5bd1db3b3c8e800e33cf3d362b957fe8af41f7
Soumik-Riyan-Madhu <soumik2010bd@gmai.com>
2026-09-12 22:11:35 +0600

    Add server startup message

--- server.js
+++ server.js
@@ -136,3 +136,6 @@ const server = http.createServer((req, res) => {
 server.listen(PORT, () => {
   console.log(`Server is running at http://localhost:${PORT}`);
 });
+
+
+console.log("Node.js Assignment 06");
